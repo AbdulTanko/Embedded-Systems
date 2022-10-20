e1d85d3f52e9cd5dd900e3f41e348a6ed0c36f51
AbdulTanko <abdul.tankosalifu@students.plymouth.ac.uk>
2022-10-20 15:49:23 +0100

    20/10/22

--- Tasks/Task-102-SOS/main.cpp
+++ Tasks/Task-102-SOS/main.cpp
@@ -5,7 +5,11 @@ using namespace uop_msb;
 #define WAIT_TIME_MS 500 
 DigitalOut greenLED(TRAF_GRN1_PIN);
 DigitalOut redLED(TRAF_RED1_PIN);
-DigitalOut yellowLED(TRAF_YEL1_PIN)
+DigitalOut yellowLED(TRAF_YEL1_PIN);
+DigitalOut redLED2 (TRAF_RED2_PIN);
+DigitalOut greenLED2(TRAF_GRN2_PIN);
+DigitalOut yellowLED2(TRAF_YEL2_PIN);
+
 
 Buzzer buzz;
 Buttons buttons;
@@ -18,7 +22,7 @@ volatile int x = 0;
 int main()
 {
     //Wait for the BLUE button to be pressed (otherwise this becomes super annoying!)
-    while (buttons.BlueButton == 1);
+    while (buttons.BlueButton == 0);
     
     //Repeat everything "forever" (until the power is removed or the chip is reset)
     while (true)
@@ -27,29 +31,64 @@ int main()
         //On for 500ms
         greenLED = 1;
         buzz.playTone("C");
-        wait_us(WAIT_TIME_MS * 1000);  //500ms
+        wait_us(WAIT_TIME_MS * 1000);  //250ms
 
         //Off for 500ms
-        greenLED = 0;
-        buzz.playTone("C", Buzzer::HIGHER_OCTAVE);
-        wait_us(WAIT_TIME_MS * 1000);  //500ms
-
+        greenLED = 0;    
 
         // on for 500ms
         yellowLED = 1;
         buzz.playTone("D");
-        wait_us(WAIT_TIME_MS * 1000);//500ms
+        wait_us(WAIT_TIME_MS * 1000);//250ms
         
         //off for 500ms
         yellowLED = 0;
-        buzz.playTone("D");
-        wait_us(WAIT_TIME_MS * 1000);  //500ms
 
+        //On for 500ms
+        redLED = 1;
+        buzz.playTone("E");
+        wait_us(WAIT_TIME_MS * 1000);  //250ms
+
+        //Off for 500ms
+        redLED = 0;
+
+       
+        //On for 500ms
+        redLED2 = 1;
+        buzz.playTone("F");
+        wait_us(WAIT_TIME_MS * 1000);  //250ms
+
+        //Off for 500ms
+        redLED2 = 0;    
+
+
+
+        // on for 500ms
+        yellowLED2 = 1;
+        buzz.playTone("F");
+        wait_us(WAIT_TIME_MS * 1000);//250ms
         
+        //off for 500ms
+        yellowLED2 = 0;
+        wait_us(WAIT_TIME_MS * 1000);  //250ms
+
+        //On for 500ms
+        greenLED2 = 1;
+        buzz.playTone("G");
+        wait_us(WAIT_TIME_MS * 1000);  //250ms
+
+        //Off for 500ms
+        greenLED2 = 0;       
+        wait_us(WAIT_TIME_MS * 1000);  //250ms
+
+
+    
+
+
 
         //Pause
         buzz.rest();
-        wait_us(WAIT_TIME_MS * 1000);
+        wait_us(WAIT_TIME_MS * 1000);        
 
     }
 }
