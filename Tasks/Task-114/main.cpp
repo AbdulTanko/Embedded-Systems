6e9b5952682bd16eb1e34d93ad814446d86dce5c
AbdulTanko <91929628+AbdulTanko@users.noreply.github.com>
2022-11-18 20:19:18 +0000

    18/11/22 part2

--- Tasks/Task-114/main.cpp
+++ Tasks/Task-114/main.cpp
@@ -1,9 +1,46 @@
 #include "mbed.h"
 
 // main() runs in its own thread in the OS
+DigitalOut redLED(PC_2);
+DigitalOut greenLED(PC_6);
+DigitalOut yellowLED(PC_3);
+
 int main()
 {
     while (true) {
+        redLED = 1; //red led on
+        wait_us(10000000);
+
+        yellowLED = 1;
+        wait_us(10000000);
+
+        redLED = 0;
+        yellowLED = 0;
+        wait_us(10000000);
+
+        greenLED = 1;
+        wait_us(10000000);
+
+        greenLED = 0;
+        wait_us(10000000);
+
+        int n = 0;
+
+        while (n<4)
+        {
+            n= n+1;
+            yellowLED = 1;
+            wait_us(10000000);
+
+            yellowLED = 0;
+            wait_us(10000000);
+
+        }
+
+    
+
+
+
 
     }
 }
