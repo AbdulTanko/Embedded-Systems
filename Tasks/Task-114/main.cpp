154a8f9acc02ff087f7db4ee9b3421566d3bacac
AbdulTanko <91929628+AbdulTanko@users.noreply.github.com>
2022-11-18 20:35:41 +0000

    task 114 to be completed

--- Tasks/Task-114/main.cpp
+++ Tasks/Task-114/main.cpp
@@ -8,25 +8,25 @@ DigitalOut yellowLED(PC_3);
 int main()
 {
     while (true) {
-        redLED = 1; //red led on
+        redLED = 1; //red led on for 1 second 
+        wait_us(10000000); 
+
+        yellowLED = 1; // red and yellow for 1 second 
         wait_us(10000000);
 
-        yellowLED = 1;
+        redLED = 0; // red led goes off
+        yellowLED = 0; //yellow led goes off
         wait_us(10000000);
 
-        redLED = 0;
-        yellowLED = 0;
+        greenLED = 1; // green led comes on for 1 second 
         wait_us(10000000);
 
-        greenLED = 1;
-        wait_us(10000000);
-
-        greenLED = 0;
+        greenLED = 0; // green led goes off
         wait_us(10000000);
 
         int n = 0;
 
-        while (n<4)
+        while (n<4) //  while l
         {
             n= n+1;
             yellowLED = 1;
